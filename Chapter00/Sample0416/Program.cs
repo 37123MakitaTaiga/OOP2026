d8cee32b51a10869abd259cac8cb65fb98a39c2a
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-04-16 11:55:15 +0900

    for文からwhile文に変更

--- Chapter00/Sample0416/Program.cs
+++ Chapter00/Sample0416/Program.cs
@@ -2,11 +2,10 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+            
             int i = 0;
-            while (i < 10)
-            {
+            while (i < 10){
                 Console.WriteLine("Hello, World!");
                 i++;
             }
