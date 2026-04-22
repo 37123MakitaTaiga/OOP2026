fcac4bcba6d201cf7a9b9de5c9c5b831cf8602b9
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-04-22 11:38:10 +0900

    エラーの表示

--- Chapter02/距離換算プログラム/DistanceConverter/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/DistanceConverter/Program.cs
@@ -2,38 +2,48 @@
 
 namespace DistanceConverter
 {
-    internal class Program
-    {
+    internal class Program{
         static void Main(string[] args){
-
-            if (args.Length >= 1 && args[0] == "-tom"){
-                PrintMeterToFeetList(int.Parse(args[1]), int.Parse(args[2]));
-            }
-            else if (args.Length >= 1 && args[0] == "-tof"){
-                PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+            if (int.TryParse(args[1], out var start) && int.TryParse(args[2], out var stop)){
+                if (args.Length == 3){
+                    if (args[0] == "-tom"){
+                        PrintMeterToFeetList(int.Parse(args[1]), int.Parse(args[2]));
+                    } else if (args[0] == "-tof"){
+                        PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+                    } else {
+                        Console.WriteLine("引数エラー");
+                    }
+                } else{
+                    Console.WriteLine("個数エラー");
+                }
             } else {
-                Console.WriteLine("引数エラー");
+                Console.WriteLine("変換エラー");
             }
         }
 
-        static void PrintMeterToFeetList(int start, int stop)
-        {
-            for (int feet = start; feet <= stop; feet++){
-                double metre = MeterToFeet(feet);
-                Console.WriteLine($"{feet}ft = {metre:0.0000}m");
+            static void PrintMeterToFeetList(int start, int stop)
+            {
+                for (int feet = start; feet <= stop; feet++)
+                {
+                    double metre = MeterToFeet(feet);
+                    Console.WriteLine($"{feet}ft = {metre:0.0000}m");
+                }
             }
-        }
-        static void PrintFeetToMeterList(int start, int stop){
-            for (int metre = start; metre <= stop; metre++){
-                double feet = FeetToMeter(metre);
-                Console.WriteLine($"{metre}m = {feet:0.0000}ft");
+            static void PrintFeetToMeterList(int start, int stop)
+            {
+                for (int metre = start; metre <= stop; metre++)
+                {
+                    double feet = FeetToMeter(metre);
+                    Console.WriteLine($"{metre}m = {feet:0.0000}ft");
+                }
             }
-        }
-        static double MeterToFeet(double feet){
-            return feet * 0.3048;
-        }
-        static double FeetToMeter(double metre){
-            return metre / 0.3048;
+            static double MeterToFeet(double feet)
+            {
+                return feet * 0.3048;
+            }
+            static double FeetToMeter(double metre)
+            {
+                return metre / 0.3048;
+            }
         }
     }
-}
---
+++ Chapter04/文字列を数値に変換する/Code4_36/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Code4_36{
+    internal class Program{
+        static void Main(string[] args){
+            var str = "1a4";
+
+            if(int.TryParse(str,out var height)) {
+                Console.WriteLine(height + "変換成功");
+            } else {
+                Console.WriteLine("変換できません");
+            }
+
+            //Parseで変更する場合は処理が必要となる
+            try{
+                int num = int.Parse(str);
+            }
+            catch (Exception){
+                Console.WriteLine("変換できません"); 
+            }
+                
+        }
+    }
+}
