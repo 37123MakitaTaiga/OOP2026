6807b774768df920633a30fc222d6726127cbffe
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-04-22 09:42:11 +0900

    コマンドライン引数の対応

---
+++ Chapter02/距離換算プログラム/DistanceConverter/DistanceConverter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace DistanceConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+
+            if (args.Length >= 1 && args[0] == "-tom"){
+                PrintMeterToFeetList(1, 10);
+            }
+            else if (args.Length >= 1 && args[0] == "-tof"){
+                PrintFeetToMeterList(1, 10);
+            } else {
+                Console.WriteLine("引数エラー");
+            }
+        }
+
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            for (int feet = start; feet <= stop; feet++){
+                double metre = MeterToFeet(feet);
+                Console.WriteLine($"{feet}ft = {metre:0.0000}m");
+            }
+        }
+        static void PrintFeetToMeterList(int start, int stop){
+            for (int metre = start; metre <= stop; metre++){
+                double feet = FeetToMeter(metre);
+                Console.WriteLine($"{metre}m = {feet:0.0000}ft");
+            }
+        }
+        static double MeterToFeet(double feet){
+            return feet * 0.3048;
+        }
+        static double FeetToMeter(double metre){
+            return metre / 0.3048;
+        }
+    }
+}
