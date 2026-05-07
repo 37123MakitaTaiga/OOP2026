8046d18b5a62af3cea464c11bddd9bc4ca5b0fde
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-05-07 12:05:16 +0900

    p57ページまで

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -2,9 +2,8 @@
     internal class Program {
         static void Main(string[] args) {
             var sales = new SalesCounter(@"data\sales.csv");
-
-            Dictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountsPerStore) {
+            var amountsPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
         }
