06a3cfa07ec168abf219b20ea0e45b7a572e1f43
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-05-11 09:44:38 +0900

    売り上集計プログラム完成

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
+
             var sales = new SalesCounter(@"data\sales.csv");
             var amountsPerStore = sales.GetPerStoreSales();
             foreach (var obj in amountsPerStore) {
