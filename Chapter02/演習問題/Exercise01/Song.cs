355f883955aa793b80bc1ff6b424a0726402e005
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-05-11 11:16:20 +0900

    2.1.4完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Song {
+        //タイトル
+        public string Title { get; set; } = string.Empty;
+        //アーティスト名
+        public string ArtistName { get; set; } = string.Empty;
+        //演奏時間
+        public int Length { get; set; }
+
+        //コンストラクター
+        public Song(string title, string artistname, int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
+}
