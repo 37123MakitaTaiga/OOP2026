c5d105afbec6021115251b8f6cf11db64c327184
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-05-13 10:56:26 +0900

    問2.2完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,7 +1,19 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            PrintMeterTonchList(1, 10);
+        }
+            static void PrintMeterTonchList(int start, int stop) {
+
+                for (int inch = start; inch <= stop; inch++) {
+                    double metre = ToMeter(inch);
+                    Console.WriteLine($"{inch}in = {metre:0.0000}m");
+                }
+            }
+        private static readonly double ratio = 0.0254;   //定数
+        //インチからメートルを求める
+        public static double ToMeter(double inch) {
+            return inch * ratio;
         }
     }
 }
