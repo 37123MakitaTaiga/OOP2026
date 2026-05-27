4ca462b35f2dd4f63317581fbec58e47fb830f71
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-05-27 10:27:22 +0900

    問4.1.1完成

--- Chapter04/nullに関するイディオム/Section04/Program.cs
+++ Chapter04/nullに関するイディオム/Section04/Program.cs
@@ -35,6 +35,19 @@
             int? amount = sale?.Amount;
             Console.WriteLine("売上高: " + amount);
             #endregion
+
+            #region 2つの要素を入れ替える
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            Console.Write("入れ替え前：");
+            Console.WriteLine("a = " + a + " b = " + b + " c = " + c);
+
+            (b, c, a, int d) = (10, 20 ,30 ,40); //入れ替え
+            
+            Console.Write("入れ替え後：");
+            Console.WriteLine("a = " + a + " b = " + b + " c = " + c);
+            #endregion
         }
 
         private static string? GetMessage(string code) {
---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+                "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+            //foreach文
+            Console.WriteLine("foreach文で出力");
+            var s = langs.Where(s => s.Contains('S'));
+            foreach (var item in s) {
+                Console.WriteLine(item);
+            }
+
+            //for文
+            Console.WriteLine("\nfor文で出力");
+            for(int i = 0; i < 9; i++) {
+                if (langs[i].Contains('S')){
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+            //while文
+            Console.WriteLine("\nwhile文で出力");
+            int j = 0;
+            while (j < 9) {
+                if (langs[j].Contains('S')) {
+                    Console.WriteLine(langs[j]);
+                }
+                j++;
+            }        
+        }
+
+        private static void Exercise2(List<string> langs) {
+            Console.WriteLine("\n --- 4.1.2 ---");
+            //LINQを使用する (Where)
+
+        }
+
+        private static void Exercise3(List<string> langs) {
+            Console.WriteLine("\n --- 4.1.3 ---");
+        }
+    }
+}
