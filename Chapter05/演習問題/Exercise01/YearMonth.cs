ee62332bb373022df600d57d1237e0d6d0577871
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-06-01 11:57:27 +0900

    5.1.3完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -9,13 +9,24 @@ namespace Exercise01 {
     public class YearMonth {
         public int Year { get; init; }
         public int Month { get; init; }
+        
+        public YearMonth (int year, int month) {
+            Year = year;
+            Month = month;
+        }
         //5.1.2
         public bool Is21Century => 2001 <= Year && Year <= 2100;
 
 
         //5.1.3
         public YearMonth AddOneMonth() {
-
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            } else {
+                return new YearMonth(Year, Month + 1);
+            }
         }
+
+        //5.1.4
     }
 }
