31d608274c7b68d0069fce7c283c22bce9b43696
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-06-15 10:36:07 +0900

    文字列の連結

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var separator = ",";
+            var result = String.Join(separator, GetWord());
+            Console.WriteLine(result);
+        }
+
+        private static IEnumerable<string> GetWord() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
