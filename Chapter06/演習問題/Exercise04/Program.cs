637ae7c7c7cb2a9d58e5620090c74b7e0544dc61
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-06-17 09:40:15 +0900

    6.4完成

--- Chapter06/演習問題/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -2,30 +2,33 @@
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var names = line.Split('=');
-            foreach(var sb in names) {
-                Console.WriteLine(ToJapanese(sb));
+            var names = line.Split('=', ';');
+            foreach (var sb in names) {
+                if (ToJapanese(sb) != "引数keyは、正しい値ではありません")
+                    Console.Write(ToJapanese(sb));
+                else
+                    Console.WriteLine(":" + sb);
+
             }
-            
-        }
-        static string ToJapanese(string key) {
-            return key switch {
-                "Novelist" => "作家",
-                "BestWork" => "代表作",
-                "Born" => "誕生年",
-                _ => "引数keyは、正しい値ではありません"
-            };
-            //古い書き方
-            //switch (key) {
-            //    case "Novelist":　
-            //        return "作家";
-            //    case "BestWork":
-            //        return "代表作";
-            //    case "Born":
-            //        return "誕生年";
-            //    default:
-            //        return "引数keyは、正しい値ではありません";
-            //}
+            static string ToJapanese(string key) {
+                return key switch {
+                    "Novelist" => "作家",
+                    "BestWork" => "代表作",
+                    "Born" => "誕生年",
+                    _ => "引数keyは、正しい値ではありません"
+                };
+                //古い書き方
+                //switch (key) {
+                //    case "Novelist":　
+                //        return "作家";
+                //    case "BestWork":
+                //        return "代表作";
+                //    case "Born":
+                //        return "誕生年";
+                //    default:
+                //        return "引数keyは、正しい値ではありません";
+                //}
+            }
         }
     }
 }
