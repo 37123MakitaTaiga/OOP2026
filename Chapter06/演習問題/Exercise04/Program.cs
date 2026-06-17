61b96cfbfc4ce88bd76eb38bb1fe1771c7ea166a
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-06-17 10:35:03 +0900

    6.4修正

--- Chapter06/演習問題/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -2,17 +2,14 @@
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var names = line.Split('=', ';');
-            foreach (var sb in names) {
-                if (ToJapanese(sb) != "引数keyは、正しい値ではありません")
-                    Console.Write(ToJapanese(sb));
-                else
-                    Console.WriteLine(":" + sb);
-
+            foreach (var word in line.Split(';')) {
+                var item = word.Split('=');
+                Console.WriteLine($"{ToJapanese(item[0])}:{item[1]}");
+                
             }
             static string ToJapanese(string key) {
                 return key switch {
-                    "Novelist" => "作家",
+                    "Novelist" => "作家の名前",
                     "BestWork" => "代表作",
                     "Born" => "誕生年",
                     _ => "引数keyは、正しい値ではありません"
