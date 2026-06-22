cfd7c75eb03a3e636316ea784639a67d040cfd4c
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-06-22 11:30:07 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,6 +1,8 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
+        public string Name { get; init; } = string.Empty;
+        public string Subject { get; init; } = string.Empty;
+        public int Score { get; init; }
 
 
     }
