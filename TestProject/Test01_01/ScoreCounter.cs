1ab009475d9a07eb150a1a508a78aa0db9864766
牧田 泰河 <oibc37123@ojs.ac.jp>
2026-06-22 11:41:25 +0900

    設問１_提出

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -11,26 +11,27 @@
         private static IEnumerable<Student> ReadScore(string filePath) {
             var sales = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            
-
-
-
-
-
-
-
-            return sales;
+            foreach (var line in lines) {
+                var items = line.Split(',');  
+                var student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(student);
+            }
+                return sales;
         }
 
         //メソッドの概要：
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
-
-
-
-
-
-
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Subject))
+                    dict[sale.Subject] += sale.Score; 
+                else
+                    dict[sale.Subject] = sale.Score;  
+            }
             return dict;
         }
     }
